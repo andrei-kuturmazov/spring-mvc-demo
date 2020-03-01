4e22413ee284d1e9b34aa715456ffaa8c88b6365
andrei_kuturmazov <andrei.kuturmazov@gmail.com>
2020-03-01 20:57:31 +0300

    first template test

---
+++ src/main/java/mvc_test/config/WebConfig.java
@@ -0,0 +1,33 @@
+package mvc_test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "mvc_test.controller")
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        FreeMarkerViewResolver freeMarkerViewResolver = new FreeMarkerViewResolver();
+        freeMarkerViewResolver.setOrder(1);
+        freeMarkerViewResolver.setPrefix("");
+        freeMarkerViewResolver.setSuffix(".ftl");
+        return freeMarkerViewResolver;
+    }
+
+    @Bean
+    public FreeMarkerConfigurer getFreeMarketConfigurer() {
+        FreeMarkerConfigurer freeMarkerConfigurer = new FreeMarkerConfigurer();
+        freeMarkerConfigurer.setTemplateLoaderPaths("/", "/WEB-INF/views/");
+        return freeMarkerConfigurer;
+    }
+
+}
